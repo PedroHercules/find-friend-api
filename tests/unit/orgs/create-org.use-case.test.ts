92e266905161c67c04b9ed79a953e4206a2c0e51
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-11 19:35:09 -0300

    test(unit): added tests on create org use case

--- tests/unit/orgs/create-org.use-case.test.ts
+++ tests/unit/orgs/create-org.use-case.test.ts
@@ -1,11 +1,19 @@
 import { MemoryOrgsRepository } from '@/repositories/in-memory/memory-orgs.repository'
+import { OrgsRepository } from '@/repositories/orgs.repository'
+import { OrgAlreadyExistsError } from '@/use-cases/errors/org-already-exists'
 import { CreateOrgUseCase } from '@/use-cases/orgs/create.use-case'
-import { describe, expect, it } from 'vitest'
+import { compare } from 'bcryptjs'
+import { beforeEach, describe, expect, it } from 'vitest'
 
-const orgsRepository = new MemoryOrgsRepository()
-const sut = new CreateOrgUseCase(orgsRepository)
+let orgsRepository: OrgsRepository
+let sut: CreateOrgUseCase
 
 describe('Create Org Use Case', () => {
+  beforeEach(() => {
+    orgsRepository = new MemoryOrgsRepository()
+    sut = new CreateOrgUseCase(orgsRepository)
+  })
+
   it('should be able to create a org', async () => {
     const { org } = await sut.execute({
       name: 'Pet Feliz',
@@ -17,5 +25,42 @@ describe('Create Org Use Case', () => {
     })
 
     expect(org.id).toEqual(expect.any(String))
+  })
+
+  it('should hash org password upon registration', async () => {
+    const { org } = await sut.execute({
+      name: 'Pet Feliz',
+      address: 'Rua Pet Feliz',
+      zipCode: '65402100',
+      email: 'petfeliz@email.com',
+      password: '123456',
+      whatsapp: '8989182912',
+    })
+
+    const isPasswordCorrectlyHashed = await compare('123456', org.password)
+
+    expect(isPasswordCorrectlyHashed).toEqual(true)
+  })
+
+  it('should not be able create org with same e-mail twice', async () => {
+    await sut.execute({
+      name: 'Pet Feliz',
+      address: 'Rua Pet Feliz',
+      zipCode: '65402100',
+      email: 'petfeliz@email.com',
+      password: '123456',
+      whatsapp: '8989182912',
+    })
+
+    const createOrgPromise = sut.execute({
+      name: 'Pet Feliz',
+      address: 'Rua Pet Feliz',
+      zipCode: '65402100',
+      email: 'petfeliz@email.com',
+      password: '123456',
+      whatsapp: '8989182912',
+    })
+
+    await expect(createOrgPromise).rejects.toBeInstanceOf(OrgAlreadyExistsError)
   })
 })
