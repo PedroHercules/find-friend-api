e0f651eeb461959415b0750160c676e2ca81d2b0
PedroHercules <pedroherculesdantas@gmail.com>
2023-06-17 18:24:11 -0300

    refactor(orgs-repository): removed findById method

--- src/repositories/orgs.repository.ts
+++ src/repositories/orgs.repository.ts
@@ -3,5 +3,4 @@ import { OrgType } from '@/@types/org'
 export interface OrgsRepository {
   create(data: OrgType): Promise<OrgType>
   findByEmail(email: string): Promise<OrgType | null>
-  findById(id: string): Promise<OrgType | null>
 }
